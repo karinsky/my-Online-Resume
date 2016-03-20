5ea40912286fb37537b2d8455aed9f0584109d26
ksk <karinsky@users.noreply.github.com>
2016-03-19 22:21:39 -0500

    fix: Location issue solved Culprit was a missing quotation mark in myHelper. Also formatting changes in css finished, insbes. in media queries, and padding for biopic slightly adjusted: 'styling' for header, footer, and bigpic sections done (incliding device testing).

--- js/myHelper.js
+++ js/myHelper.js
@@ -4,7 +4,7 @@ var HTMLmobile = '<li class="flex-item"><span class="caps">Mobile</span><span cl
 var HTMLemail = '<li class="flex-item"><span class="caps">Email</span><span class="bold">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="caps">Github</span><span class="bold">%data%</span></li>';
 var HTMLlinkedin = '<li class="flex-item"><span class="caps">Linkedin</span><span class="bold">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="caps">Location</span><span class="bold>%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="caps">Location</span><span class="bold">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<p id="welcome" class="quote white">%data%</p>';
